60d5322c9d113ccc599fdff3087c819a78a3ee28
Hor Norin <norin.hor@gmail.com>
2015-03-05 13:29:43 +0700

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Entv::Application.config.secret_key_base = 'fbe3f6f8f4f53da31d0e6828213c1c4a8e2fc73fcea61238cc97ebace1ddf610ca6bdfbd85f703b025a1a50ef0cab79a2392345426e9fee65075a7dfe4ac4d6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Entv::Application.config.session_store :cookie_store, key: '_entv_session'
