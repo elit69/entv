15fa5b04b0fb430e0411af89f588cc596efe73fd
Hor Norin <norin.hor@gmail.com>
2015-03-17 17:28:18 +0700

    implement product searching

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.where(company: params[:company])
+    if params[:company].nil? || params[:category_id].nil?
+      @products = Product.where(company: params[:company])
+    else
+      category = Category.find params[:category_id]
+      @products = category.products.where "company LIKE ?", "#{params[:company]}%"
+    end
   end
+  
   def show
     @categories = Category.all
     @product = Product.find(params[:id])
